9645ceead3876891eb0c9e557f3fa45794d0347a
Anatoliy Adamitskiy <anato1iy@fullsail.edu>
2014-07-10 20:32:09 -0400

    changes

--- AVF_1407_Adamitskiy_Anatoliy_Week1/Resources/data.js
+++ AVF_1407_Adamitskiy_Anatoliy_Week1/Resources/data.js
@@ -4,12 +4,74 @@
  */
 
 if (Ti.Network.online === true) {
-	
+
 	var remoteResponse = function() {
-		
+
 		Ti.API.debug(this.responseText);
 		var json = JSON.parse(this.responseText);
+		
+		var location = Ti.UI.createLabel({
+			text: json.current_observation.display_location.full,
+			font: {fontSize: 44, fontWeight: "bold"},
+			top:100,
+			color: "white"
+		});
+		
+		var currentWeather = Ti.UI.createImageView({
+			image: "images/weather@2x_03.png",
+			height: 350,
+			width: 350,
+			top: 170
+		});
+		
+		var nowcast = Ti.UI.createLabel({
+			text: json.current_observation.nowcast,
+			font: {fontSize: 20, fontWeight: "bold"},
+			color: "#fff",
+			bottom: 40
+		});
+		
+		var update = Ti.UI.createLabel({
+			text: json.current_observation.observation_time,
+			font: {fontSize: 20, fontWeight: "bold"},
+			color: "#fff",
+			top: 60
+		});
+		
+		var far = Ti.UI.createLabel({
+			text: json.current_observation.temp_f + "F / " + json.current_observation.temp_c + "C",
+			font: {fontSize: 44, fontWeight: "bold"},
+			color: "#fff",
+			bottom: 340
+		});
 
+		// var info1 = json.current_observation;
+
+		// Ti.API.info(info1.display_location.full);
+		// Ti.API.info(info1.temp_f);
+		// Ti.API.info(info1.temp_c);
+
+		// var current = {
+			// location : info1.display_location.full,
+			// tempF : info1.temp_f,
+			// tempC : info1.temp_c
+		// };
+// 
+		// console.log(current.location);
+		// console.log(current.tempF);
+		// console.log(current.tempC);
+// 	
+	// exports.data = current;
+	
+	if(platform == 'android'){
+		currentWeather.top = 220;
+	}
+	
+		mainWindow.add(far);
+		mainWindow.add(update);
+		mainWindow.add(nowcast);
+		mainWindow.add(currentWeather);
+		mainWindow.add(location);
 	};
 
 	var remoteError = function(e) {
@@ -33,7 +95,7 @@ if (Ti.Network.online === true) {
 	xhr.send();
 
 } else {
-	
+
 	alert("Please connect to a Wi-Fi network or cellular data to use this application.");
-	
+
 }
--- AVF_1407_Adamitskiy_Anatoliy_Week1/Resources/sqlLite.js
+++ AVF_1407_Adamitskiy_Anatoliy_Week1/Resources/sqlLite.js
@@ -1,15 +1,21 @@
 //	Opening my SQLite Database
-var db = Titanium.Database.open("database");
-db.execute("CREATE TABLE IF NOT EXISTS database (id INTEGER PRIMARY KEY, current TEXT)");
-
+// var db = Titanium.Database.open("database");
+// db.execute("CREATE TABLE IF NOT EXISTS database (id INTEGER PRIMARY KEY, current TEXT)");
+// 
 
 //	This is the big function that creates and gets the
 //	data to populate the mainWindow
-exports.getRows = function() {
-	db.execute("SELECT * FROM database");
-};
+// exports.getRows = function() {
+	// db.execute("SELECT * FROM database");
+// };
+// 
+// 
+// exports.update = function(current) {
+	// db.execute("INSERT INTO database (current) VALUES(?)", current);
+// };
 
-
-exports.update = function(current) {
-	db.execute("INSERT INTO database (current) VALUES(?)", current);
-};
+//	This function deletes a selected row from the
+//	database
+// exports.destroy = function(id) {
+	// db.execute("DELETE FROM database WHERE id = ?", id);	
+// };
